e14c5a8973f7755cce572ca6d75e0dc88f9e576a
vbegunova <veronikabegunova42@gmail.com>
2023-04-25 08:15:33 +0300

    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".js-gallery");
+const markup = galleryItems
+    .map(
+        ({ preview, original, description }) =>
+            `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+    )
+    .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+gallery.addEventListener("click", onClick);
+
+function onClick(evt) {
+    evt.preventDefault();
+
+    const { target } = evt;
+    if (!target.closest(".gallery__item")) {
+        return;
+    }
+
+    const originalImage = target.dataset.source;
+    const currentImage = galleryItems.find(
+        ({ original }) => original === originalImage
+    );
+
+    const instance = basicLightbox.create(`
+        <img src="${currentImage.original}" alt="${currentImage.description}">
+    `);
+    instance.show();
+
+    if (instance.visible()) {
+        document.addEventListener("keydown", escapeModal);
+    }
+    
+    function escapeModal(evt) {
+        if (evt.key === "Escape") {
+            instance.close();          
+            document.removeEventListener("keydown", escapeModal);
+        }
+    }
+}
+
+
